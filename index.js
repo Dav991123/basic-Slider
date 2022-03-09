c52d65dd554c4e5314536dc4de3b86251a50d0d9
Dav991123 <davit.sargsyan@deverno.co>
2022-03-09 18:27:46 +0400

    feat: add slider

---
+++ index.js
@@ -0,0 +1,14 @@
+
+
+
+
+const array = ['red', 'blue', 'black', 'indigo'];
+const lastValue = array[array.length - 1];
+console.log(lastValue); //indigo
+
+
+
+
+
+// const lastValue = array.at(-1);
+// console.log(lastValue); //indigo
